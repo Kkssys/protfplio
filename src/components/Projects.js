56745e48ceaef7c98c8d8b26bc76b12e27abcc4c
Dinesh_G <166093092+Kkssys@users.noreply.github.com>
2026-08-05 10:49:48 +0530

    project1

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -93,12 +93,12 @@ function Projects({ projects }) {
                   </div>
                   <div className="project-content">
                     <h3 className="project-title">{project.title}</h3>
-                    <p className="project-description">{project.description}</p>
+                    {/* <p className="project-description">{project.description}</p>
                     <div className="project-technologies">
                       {project.technologies.map((tech, index) => (
                         <span key={index} className="tech-tag">{tech}</span>
                       ))}
-                    </div>
+                    </div> */}
                     <div className="project-links">
                       {project.github && (
                         <a href={project.github} target="_blank" rel="noopener noreferrer" className="btn btn-small">
